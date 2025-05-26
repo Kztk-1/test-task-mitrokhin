f03ebc42db8a13779f2fb1d0305c03fef7b253b8
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-26 20:01:18 +0300

    Initial commit

---
+++ test-task/src/main/java/com/kztk/test_task/TelegramAuthApplication.java
@@ -0,0 +1,14 @@
+package com.kztk.test_task;
+
+import com.kztk.test_task.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramAuthApplication.class, args);
+	}
+
+}
